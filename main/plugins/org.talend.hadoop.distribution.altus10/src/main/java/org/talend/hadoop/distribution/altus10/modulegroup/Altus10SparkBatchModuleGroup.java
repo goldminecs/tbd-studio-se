a849549755d7ed54fab298aeda96093232d8422d
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-12-13 18:07:25 +0100

    feat(TBD5567) : Make cloud storage dependencies optional

--- main/plugins/org.talend.hadoop.distribution.altus10/src/main/java/org/talend/hadoop/distribution/altus10/modulegroup/Altus10SparkBatchModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.altus10/src/main/java/org/talend/hadoop/distribution/altus10/modulegroup/Altus10SparkBatchModuleGroup.java
@@ -16,12 +16,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.altus10.Altus10Constant;
 import org.talend.hadoop.distribution.condition.BasicExpression;
 import org.talend.hadoop.distribution.condition.ComponentCondition;
 import org.talend.hadoop.distribution.condition.EqualityOperator;
 import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
 import org.talend.hadoop.distribution.constants.SparkBatchConstant;
-import org.talend.hadoop.distribution.altus10.Altus10Constant;
 
 public class Altus10SparkBatchModuleGroup {
 
@@ -29,11 +29,20 @@ public class Altus10SparkBatchModuleGroup {
                                                            SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER, EqualityOperator.EQ,
                                                            "false")); //$NON-NLS-1$
 
+    private final static ComponentCondition conditionS3 = new SimpleComponentCondition(new BasicExpression(
+            SparkBatchConstant.ALTUS_CLOUD_PROVIDER, EqualityOperator.EQ, "ALTUS_AWS_CLOUD")); //$NON-NLS-1$
+
+    private final static ComponentCondition conditionAzureFS = new SimpleComponentCondition(new BasicExpression(
+            SparkBatchConstant.ALTUS_CLOUD_PROVIDER, EqualityOperator.EQ, "ALTUS_AZURE_CLOUD")); //$NON-NLS-1$
+
  public static Set<DistributionModuleGroup> getModuleGroups() {
      Set<DistributionModuleGroup> hs = new HashSet<>();
      hs.add(new DistributionModuleGroup(Altus10Constant.HDFS_MODULE_GROUP.getModuleName(), false, condition));
      hs.add(new DistributionModuleGroup(Altus10Constant.MAPREDUCE_MODULE_GROUP.getModuleName(), false, condition));
      hs.add(new DistributionModuleGroup(Altus10Constant.SPARK_MODULE_GROUP.getModuleName(), false, condition));
+        hs.add(new DistributionModuleGroup(Altus10Constant.SPARK_S3_MRREQUIRED_MODULE_GROUP.getModuleName(), true, conditionS3));
+        hs.add(new DistributionModuleGroup(Altus10Constant.SPARK_AZURE_MRREQUIRED_MODULE_GROUP.getModuleName(), true,
+                conditionAzureFS));
      hs.add(new DistributionModuleGroup(Altus10Constant.BIGDATALAUNCHER_MODULE_GROUP.getModuleName(), true, condition));
      return hs;
  }
